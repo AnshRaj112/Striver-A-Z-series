71af6adba002a9cebdf29a8f2246ac12a16532ee
AnshRaj112 <anshraj112@gmail.com>
2026-05-01 18:38:57 +0530

    Making the code a bit complex and having few more things

--- array.cpp
+++ array.cpp
@@ -8,18 +8,32 @@ int main(){
 
     if (n < 0){
         cout << "Enter the value greater than 0";
+        return 1;
     }
 
     vector<int>arr(n);
 
+    //Having the values inside the array
     cout << "Enter the values for the array\n";
     for (int i = 0; i < n; ++i){
         cout << "Element " << i << " ";
         cin >> arr[i];
     }
+
+    //Printing out the elements of the array
     cout << "\nThe elements of the array are: ";
     for (int i = 0; i < n; ++i){
         cout << arr[i] << " ";
+    }
+
+    //Printing out the specific element of the array
+    int m;
+    cout << "\nEnter the position you want to find: ";
+    cin >> m;
+    if (m >= n){
+        cout << "Please enter the position lesser the size of array";
+    } else{
+        cout << "The element at " << m << "th position is : " << arr[m];
     }
     return 0;
 }
